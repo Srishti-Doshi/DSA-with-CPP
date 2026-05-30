c7ca2de10ca5254e4b1fa64ed7426d6072021003
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-05-30 16:45:00 +0530

    post and pre increment difference

---
+++ Cpp_Basics/7.Conditional_statmnts/Q4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    cout<<++a<<endl;
+    int b = 5;
+    cout<<b++<<endl;
+    cout << a;
+    cout << b;
+    return 0;
+}
