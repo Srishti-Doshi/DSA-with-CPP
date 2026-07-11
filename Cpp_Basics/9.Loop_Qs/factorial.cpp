3cc36ce42cb83cfa5f810cb0ddb3909f3523849a
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-07-12 04:46:03 +0530

    Factorial program using for loop

--- Cpp_Basics/9.Loop_Qs/factorial.cpp
+++ Cpp_Basics/9.Loop_Qs/factorial.cpp
@@ -1,2 +1,19 @@
-// Question1: WAP to find the Factorial of a number entered by the user. Hint: factorial of a number(n) = n*(n-1)*(n-2)*(n-3)*......* 1 and exists for positive numbers only. W ewrite factorial as n! So, factorial of0! = 1, 1!=1, 2!=2, 3!=6, 4!=24 and so on.Note- Please do not confuse factorial with NOTE QUAL TO operator, they are not the same
+// Question1: WAP to find the Factorial of a number entered by the user. Hint: factorial of a number(n) = n*(n-1)*(n-2)*(n-3)*......* 1 and exists for positive numbers only. We write factorial as n! So, factorial of 0! = 1, 1!=1, 2!=2, 3!=6, 4!=24 and so on.Note- Please do not confuse factorial with NOTE QUAL TO operator, they are not the same
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int fact = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    cout<<"factorial of n: "<<fact;
+    return 0;
+}
