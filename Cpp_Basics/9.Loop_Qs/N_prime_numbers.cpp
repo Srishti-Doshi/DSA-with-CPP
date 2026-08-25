d916200f67fa91bd1fd82447c8d0771821b99c64
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-08-25 16:57:41 +0530

    print prime numbers from 2 to N

---
+++ Cpp_Basics/9.Loop_Qs/N_prime_numbers.cpp
@@ -0,0 +1,37 @@
+/*
+For a positive N, write a program that prints the prime numbers from 2 to N.
+Assume N >= 2.
+
+A prime number is a positive integer greater than 1 that has exactly two factors: 1 and The number itself
+Examples
+2 → factors: 1, 2 → ✅ Prime
+11 → factors: 1, 3 → ✅ Prime
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: ";
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++)    // O(sqrt(n))
+    {
+        bool isPrime = true;
+        for (int j = 2; j*j <= i; j++)
+        {
+            if (i%j == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            cout<<i<<" ";
+        }
+
+    }
+
+    return 0;
+}
