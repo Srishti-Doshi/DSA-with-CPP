81d99036859461073d57eec49a498a208c5781f2
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-08-27 12:27:01 +0530

    Print Hollow Rectangle Pattern

--- Cpp_Basics/10.Patterns/06_hollow_rectangle_2.cpp
+++ Cpp_Basics/10.Patterns/06_hollow_rectangle_2.cpp
@@ -21,7 +21,7 @@ int main()
 
     for(int i = 1; i <= n; i++)
     {
-        cout<<"* ";
+        cout<<"* ";  // first star
 
         for(int j = 1; j <= n-1; j++)
         {
@@ -35,7 +35,7 @@ int main()
             }
         }
 
-        cout<<"* "<<endl;
+        cout<<"* "<<endl;  // last star
     }
 
     return 0;
