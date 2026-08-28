1dd0387f79c040f8e1a1b65b201a50eeb9afea24
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-08-28 08:29:34 +0530

    Print Floyd's Triangle Pattern

---
+++ Cpp_Basics/10.Patterns/08_Floyd_s_triangle.cpp
@@ -0,0 +1,33 @@
+/*
+Print Floyd's Triangle Pattern
+
+n = 4
+
+1
+23
+456
+78910
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter no. of rows: ";
+    cin>>n;
+    int num = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout<<num++<<" ";
+        }
+
+        cout<<endl;
+    }
+    
+    return 0;
+}
