721617eb81a23ca1620da45fd1c88c5ce5f678e1
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-08-29 14:08:20 +0530

    Half Diamond Pattern

---
+++ Cpp_Basics/10.Patterns/12_half_diamond.cpp
@@ -0,0 +1,42 @@
+/*
+Half Diamond Pattern
+
+*
+* *
+* * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    int m = n / 2;
+
+    for (int i = 1; i <= m + 1; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<"* ";
+        }
+        cout << endl;
+    }
+
+    for (int i = m; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<"* ";
+        }
+        cout << endl;
+    }
+}
