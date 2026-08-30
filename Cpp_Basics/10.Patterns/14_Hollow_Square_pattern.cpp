9b6ae0565649c3fc9cf9d1fc570e02cf51f71ca7
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-08-30 12:27:07 +0530

    Hollow Square Pattern

---
+++ Cpp_Basics/10.Patterns/14_Hollow_Square_pattern.cpp
@@ -0,0 +1,47 @@
+/*
+
+* * * *
+* - - *
+* - - *
+* * * *
+
+* * * * * *
+* - - - - *
+* - - - - *
+* - - - - *
+* - - - - *
+* * * * * *
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(i==1 || i==n)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                cout<<"* ";
+            }
+        }
+        else
+        {
+            cout<<"* ";
+            for(int j = 1; j <= n-2; j++)
+            {
+                cout<<"  ";
+            }
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+}
