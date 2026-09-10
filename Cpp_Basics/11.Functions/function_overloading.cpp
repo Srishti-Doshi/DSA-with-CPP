d0255671be1dff0d1a355912b3044b919a549de6
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-09-10 17:34:13 +0530

    function overloading

--- Cpp_Basics/11.Functions/function_overloading.cpp
+++ Cpp_Basics/11.Functions/function_overloading.cpp
@@ -1,7 +1,15 @@
+// Multiple functions with the same name but different number/type of parameters
+
+
 #include<iostream>
 using namespace std;
 
 int sum(int a, int b)
+{
+    return a + b;
+}
+
+int sum(double a, double b)
 {
     return a + b;
 }
@@ -14,6 +22,7 @@ int sum(int a, int b, int c)
 int main()
 {
     cout<<sum(2, 3)<<endl;
+    cout<<sum(2.5, 3.5)<<endl;
     cout<<sum(2, 3, 4)<<endl;
     return 0;
 }
