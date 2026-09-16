dd7c5f02a4e643e4f677fe5666b0f376fd0c7a22
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-09-16 07:12:54 +0530

    array

--- 10.Arrays/p1.cpp
+++ 10.Arrays/p1.cpp
@@ -1,13 +1,30 @@
+//Output & Input array
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int arr[5] = {10, 20, 30, 40, 50};
+    int arr[5];
+    int len = sizeof(arr)/sizeof(int);
+
+    //input array
+    cout<<"Input array elements: ";
+
+    for(int i = 0; i < len; i++)
+    {
+        cin>>arr[i];
+    }
+  
+    cout<<endl;
+
+
+    //output array
+
 
     cout << "Array elements are:\n";
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < len; i++)
     {
         cout << arr[i] << " ";
     }
