9260653e0eb7e2168d9b41eec63dd238cf847aa5
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-09-17 22:20:11 +0530

    array passed as reference using pointers

--- 10.Arrays/p5_array_passsed_by_reference_behaviour.cpp
+++ 10.Arrays/p5_array_passsed_by_reference_behaviour.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void printArr(int arr[], int n)   //arr is pointer
+void printArr(int arr[], int n)   //arr becomes a pointer to the first element of given array
 {
     
-    cout<<"size of int pointer: "<<sizeof(arr)<<"\n";               // 4 = size of int pointer
+    cout<<"size of int pointer: "<<sizeof(arr)<<"\n";               // 4 = size of int pointer in 32-bit program, pointers are commonly 4 bytes, 8 bytes in 64 bit program
     
     // int n = sizeof(arr)/sizeof(int);  // 1 = 4/4
 
 
+    cout<<arr<<endl;
+    cout<<*arr<<endl;
+
     cout<<"Array: \n";
     for(int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<", ";
+        cout<<arr[i]<<", ";  //C++ defines: arr[i] as: *(arr + i)  //automatically accounts for the size  // 1001 + 1 = 1004, 1001 + 2 = 1008
     }
 }
 
